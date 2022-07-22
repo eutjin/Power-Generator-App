0fc28ef92d31d0352890db2fecdca713f7e9783f
eutjin <91615591+eutjin@users.noreply.github.com>
2022-07-22 23:58:10 +0900

    build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+
+const path= require('path')
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
@@ -12,5 +14,14 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/daily', require('./routes/dailyRoutes'))
 app.use('/api/consumption', require('./routes/consumptionRoutes'))
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
+else{
+    app.get('/', (req,res)=>res.send('set!'))
+}
 
 app.listen(port, ()=>console.log(`server started on port ${port}`))
